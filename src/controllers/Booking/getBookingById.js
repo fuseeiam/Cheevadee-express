528a849c6f22e214f5262461c02fbb1c4524cf38
fuseeiam <nattaporn.orr@gmail.com>
2023-11-19 16:42:29 +0700

    booking card

---
+++ src/controllers/Booking/getBookingById.js
@@ -0,0 +1,35 @@
+const prisma = require('../../models/prisma');
+const createError = require('../../utils/create_error');
+
+exports.getBookingByID = async (req, res, next) => {
+    try {
+        const bookingDetails = await prisma.booking.findMany({
+            where: {
+                userId: req.user.id
+            },
+            include: {
+                user: {
+                    select: {
+                        firstName: true,
+                        lastName: true,
+                        email: true
+                    }
+                },
+                room: {
+                    select: {
+                        bed: true,
+                        picture: true,
+                        roomtype: true,
+                        view: true,
+                        price: true
+                    }
+                },
+
+            },
+        })
+        // console.log(bookingDetails);
+        res.status(200).json({ bookingDetails })
+    } catch (error) {
+        next(error)
+    }
+}
--- src/routes/book-route.js
+++ src/routes/book-route.js
@@ -1,10 +1,11 @@
 
 const express = require('express');
 const authenticatedMiddleware = require('../middlewares/authenticated');
-const { createBooking } = require("../controllers/Booking/createBooking")
+const { createBooking, getBookingById } = require("../controllers/Booking/createBooking")
 const uploadMiddleware = require('../middlewares/upload');
 const router = express.Router();
 const postController = require("../../src/controllers/Booking/PostSlipBooking");
+const { getBookingByID } = require('../controllers/Booking/getBookingById');
 
 // router.post('/reserve', authenticatedMiddleware, createBooking);
 router.post('/reserve',
@@ -12,6 +13,8 @@ router.post('/reserve',
     uploadMiddleware.single('paymentSlip'),
     createBooking
 )
+router.get("/history", authenticatedMiddleware, getBookingByID)
+
 
 
 module.exports = router;
